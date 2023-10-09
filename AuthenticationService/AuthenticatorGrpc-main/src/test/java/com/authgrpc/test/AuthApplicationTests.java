c52cedc06cb06a1fb6eb14e3b8fba72f6113fb13
Karthik Shet <54632000+thekarthikshet@users.noreply.github.com>
2023-10-09 15:29:54 +0530

    Update AuthApplicationTests.java

--- AuthenticationService/AuthenticatorGrpc-main/src/test/java/com/authgrpc/test/AuthApplicationTests.java
+++ AuthenticationService/AuthenticatorGrpc-main/src/test/java/com/authgrpc/test/AuthApplicationTests.java
@@ -2,7 +2,7 @@ package com.authgrpc.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//Test class
 @SpringBootTest
 class AuthApplicationTests {
 
